1ff57c884eea743245265d21812842bdc5c84780
high-moctane <high.moctane@gmail.com>
2019-05-29 17:00:31 +0900

    async/awaitからThreadPoolExecutorに変更した

--- bluetooth_server/main.py
+++ bluetooth_server/main.py
@@ -1,11 +1,7 @@
 import os
-from dotenv import load_dotenv
 import server
 
 if __name__ == "__main__":
-    # .env の読み込み
-    load_dotenv()
-
     # サーバをたてる
     server.Server(server_name=os.getenv("SERVER_NAME"),
                   port=int(os.getenv("PORT"))).run()
--- bluetooth_server/server.py
+++ bluetooth_server/server.py
@@ -1,5 +1,7 @@
-import asyncio
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+import sys
+import traceback
 
 import errors as err
 import search
@@ -22,40 +24,33 @@ class Handler(BaseHTTPRequestHandler):
             self.end_headers()
 
             # サーチする
-            s = search.Search(bd_addrs)
-            loop = asyncio.get_event_loop()
-            exist_bd_addrs = loop.run_until_complete(s.run())
-            loop.close()
+            s = search.Search(bd_addrs, max_workers=int(
+                os.getenv("MAX_WORKERS")))
+            exist_bd_addrs = s.run()
 
             # 返事する
             response_body = "\n".join(exist_bd_addrs)
             self.wfile.write(response_body.encode())
 
-        except err.BluetoothServerError as e:
+        except Exception:
+            t, v, tb = sys.exc_info()
+            trace = traceback.format_exception(t, v, tb)
+            print("\n".join(trace))
+
             # ヘッダを作る
-            self.send_response(200)
+            self.send_response(400)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
 
             # 返事する
-            response_body = "BluetoothServerError\n" + str(e)
-            self.wfile.write(response_body.encode())
-
-        except Exception as e:
-            # ヘッダを作る
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-
-            # 返事する
-            response_body = "Error\n" + str(e)
+            response_body = "Error\n" + "\n".join(trace)
             self.wfile.write(response_body.encode())
 
 
 class Server:
     """サーバ。"""
 
-    def __init__(self, server_name="localhost", port="8080"):
+    def __init__(self, server_name="localhost", port=8080):
         self.server_name = server_name
         self.port = port
 
