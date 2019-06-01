6d4a268eb38aa7cb57bc641280c509bceeacc602
high-moctane <high.moctane@gmail.com>
2019-06-01 17:50:04 +0900

    if 文を圧縮

--- milbot/utils.py
+++ milbot/utils.py
@@ -1,7 +1,4 @@
 def is_bot_message(data):
     """送られてきた message が bot によるものかどうかを判定する。"""
 
-    if "bot_message" in data.values():
-        return True
-    else:
-        return False
+    return "bot_message" in data.values()
