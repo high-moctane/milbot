fbeb6ecce906180e40cc4918f426fc92b9aef532
high-moctane <high.moctane@gmail.com>
2019-06-04 20:07:28 +0900

    便利なシェルスクリプトを作った

---
+++ bin/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+systemd start bluetooth_server
+systemd start milbot-redis
+systemd start milbot
---
+++ bin/uninstall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Systemd のサービスファイルの削除
+echo "removing systemd service files"
+rm /etc/systemd/bluetooth_server.service
+rm /etc/systemd/milbot-redis.service
+rm /etc/systemd/bluetooth_server.service
+echo "done"
+
+# milbot のコンテナを消す
+if [ $(docker ps -a | grep milbot) ]
+then
+    echo "removing milbot container"
+    docker rm milbot
+    echo "done"
+fi
+# milbot-redis のコンテナを消す
+if [ $(docker ps -a | grep milbot-redis) ]
+then
+    echo "removing milbot-redis container"
+    docker rm milbot-redis
+    echo "done"
+fi
+
+# milbot のイメージを消す
+if [ $(docker images | grep milbot) ]
+then
+    echo "removing milbot image"
+    docker rmi milbot
+    echo "done"
+fi
+# milbot-redis のイメージを消す
+if [ $(docker images | grep milbot-redis) ]
+then
+    echo "removing milbot-redis image"
+    docker rmi milbot-redis
+    echo "done"
+fi
