79df888f7593fc6cd71fc28adbb83fbc9871a4aa
high-moctane <high.moctane@gmail.com>
2019-06-05 15:48:18 +0900

    環境変数ではなくコマンドラインオプションで値を受け取ることにした

--- bluetooth_server/main.py
+++ bluetooth_server/main.py
@@ -1,7 +1,13 @@
+import argparse
 import os
 import server
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--address", help="an address of server", default="0.0.0.0")
+    parser.add_argument("--port", help="port number", type=int, default=8080)
+    args = parser.parse_args()
+
     # サーバをたてる
-    server.Server(server_name=os.getenv("SERVER_NAME"),
-                  port=int(os.getenv("PORT"))).run()
+    server.Server(server_name=args.address, port=args.port).run()
