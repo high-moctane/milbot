a7f5a0e31a50eec0e681e1c283599771a8eebb68
high-moctane <high.moctane@gmail.com>
2019-06-05 16:57:23 +0900

    milbot を起動するシェルスクリプトを作った

---
+++ bin/start_milbot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ip_addr=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
+/usr/bin/docker run --pids-limit 30 --ulimit fsize=100000000:100000000 --ulimit nosize=256 --init --name milbot --rm --memory=250m --memory-swappiness=0 --add-host=host_address:${ip_addr} milbot
