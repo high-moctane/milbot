a3ee18d021bb02105016a016de3bb7c05a955a7a
high-moctane <high.moctane@gmail.com>
2019-06-13 15:35:48 +0900

    タイマーで終了する機能をつけた

--- milbot/main.py
+++ milbot/main.py
@@ -6,6 +6,7 @@ import certifi
 import os
 import slack
 import ssl as ssl_lib
+import sys
 
 from log import Log
 
@@ -16,6 +17,11 @@ if __name__ == "__main__":
     token = os.getenv("SLACK_API_TOKEN")
     loop = asyncio.get_event_loop()
     asyncio.set_event_loop(loop)
+
+    # 自動再起動の設定
+    sec = int(os.getenv("RESTART_HOUR")) * 3600
+    loop.call_later(sec, sys.exit, 1)
+
     try:
         ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
         rtm_client = slack.RTMClient(
