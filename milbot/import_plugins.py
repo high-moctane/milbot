811d6719f1887eb4c7fe1e02c33844dbae1a8d64
high-moctane <high.moctane@gmail.com>
2019-06-19 18:05:59 +0900

    575警察をとりあえず読み込まないことにした

--- milbot/import_plugins.py
+++ milbot/import_plugins.py
@@ -1,7 +1,7 @@
 # 読み込むプラグインをここに書く
 import plugins.atnd
 import plugins.exit
-import plugins.fixed_verse
+# import plugins.fixed_verse
 import plugins.hello
 import plugins.help
 import plugins.kitakunoki
