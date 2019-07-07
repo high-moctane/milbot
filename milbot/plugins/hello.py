1530700f172b92526267b74f1cf1ae9bed0ea8a7
high-moctane <high.moctane@gmail.com>
2019-07-08 01:35:22 +0900

    golang に書き換えのため milbot 以下を全削除

--- milbot/plugins/hello.py
+++
@@ -1,9 +0,0 @@
-import slack
-
-from log import Log
-
-
-@slack.RTMClient.run_on(event="hello")
-async def hello(**payload):
-    """つながったことを表示する"""
-    Log.info("RTM connected")
--- milbot/utils.py
+++
@@ -1,4 +0,0 @@
-def is_bot_message(data):
-    """送られてきた message が bot によるものかどうかを判定する。"""
-
-    return "bot_message" in data.values()
