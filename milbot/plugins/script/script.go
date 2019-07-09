ff97dc7aa5d64957b9d361f3c2992c9c0b931e1f
high-moctane <high.moctane@gmail.com>
2019-07-09 20:15:36 +0900

    script を実装

--- milbot/plugins/script/script.go
+++ milbot/plugins/script/script.go
@@ -2,6 +2,10 @@ package script
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"regexp"
 	"strings"
 
@@ -54,7 +58,12 @@ func (p Plugin) Stop() error {
 func help(api *slack.Client, ev *slack.MessageEvent) {
 	botutils.LogEventReceive(api, ev, "atnd help")
 
-	mes := "" // TODO
+	mes := "スクリプトを走らせます。\n" +
+		"現在以下のコマンドを受け付けています。\n" +
+		"    `milbot bash`\n" +
+		"    `milbot python3`\n" +
+		"コマンドに続けてスクリプトを入力してください。" +
+		"コードを ``` で囲んでも動きます。"
 
 	botutils.SendMessageWithLog(api, ev, mes)
 }
@@ -64,28 +73,31 @@ func bash(api *slack.Client, ev *slack.MessageEvent) {
 	input, err := parseInput(ev.Text)
 	if err != nil {
 		botutils.SendMessageWithLog(api, ev, "入力フォーマットが不正です(´･ω･｀)")
+		return
 	}
-	botutils.SendMessageWithLog(api, ev, input)
-	// out, err := run([]string{"bash", "main.sh"}, "main.sh", input)
-	// if err != nil {
-	// 	botutils.SendMessageWithLog(api, ev, "実行に失敗しました(´; ω ;｀)")
-	// 	botutils.LogBoth("script: bash error: ", err)
-	// 	return
-	// }
-	// botutils.SendMessageWithLog(api, ev, out)
+	out, err := run([]string{"bash"}, "main.sh", input)
+	if err != nil {
+		botutils.SendMessageWithLog(api, ev, "実行に失敗しました(´; ω ;｀)\n"+out)
+		botutils.LogBoth("script: bash error: ", err)
+		return
+	}
+	botutils.SendMessageWithLog(api, ev, out)
 }
 
 // python3 を走らせる
-func python3(api *slack.Client, ev *slack.MessageEvent) (string, error) {
-	// input := parseInput(ev.Text)
-	// out, err := run([]string{"python3", "main.py"}, "main.py", input)
-	// if err != nil {
-	// 	botutils.SendMessageWithLog(api, ev, "実行に失敗しました(´; ω ;｀)")
-	// 	botutils.LogBoth("script: bash error: ", err)
-	// 	return
-	// }
-	// botutils.SendMessageWithLog(api, ev, out)
-	return "", nil
+func python3(api *slack.Client, ev *slack.MessageEvent) {
+	input, err := parseInput(ev.Text)
+	if err != nil {
+		botutils.SendMessageWithLog(api, ev, "入力フォーマットが不正です(´･ω･｀)")
+		return
+	}
+	out, err := run([]string{"python3", "-B"}, "main.py", input)
+	if err != nil {
+		botutils.SendMessageWithLog(api, ev, "実行に失敗しました(´; ω ;｀)\n"+out)
+		botutils.LogBoth("script: python3 error: ", err)
+		return
+	}
+	botutils.SendMessageWithLog(api, ev, out)
 }
 
 // parseInput は送られたメッセージからスクリプトの部分を取り出す
@@ -97,8 +109,48 @@ func parseInput(text string) (string, error) {
 		return "", fmt.Errorf("not found body")
 	}
 
-	body := text[indices[1][1]:]
+	body := text[indices[1][1]-1:]
 	body = strings.TrimPrefix(body, "```")
 	body = strings.TrimSuffix(body, "```")
 	return body, nil
+}
+
+// run で実際にコマンドを走らせる
+func run(cmd []string, fname, script string) (string, error) {
+	dir, err := ioutil.TempDir("", "milbot_script")
+	if err != nil {
+		return "", err
+	}
+	defer os.RemoveAll(dir)
+
+	abspath := filepath.Join(dir, fname)
+	if err := writeScript(abspath, script); err != nil {
+		return "", err
+	}
+
+	var c *exec.Cmd
+	if len(cmd) == 1 {
+		c = exec.Command(cmd[0], abspath)
+	} else {
+		c = exec.Command(cmd[0], append(cmd[1:], abspath)...)
+	}
+	out, err := c.CombinedOutput()
+	if err != nil {
+		return string(out), err
+	}
+	return string(out), err
+}
+
+// writeScript で abspath に script が書かれたファイルを作ります
+func writeScript(abspath, script string) error {
+	f, err := os.Create(abspath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(script); err != nil {
+		return err
+	}
+	return nil
 }
