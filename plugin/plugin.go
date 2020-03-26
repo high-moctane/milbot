cdb84ae3ad4c5d4785337efcf0439d48c8f3bd66
high-moctane <high.moctane@gmail.com>
2020-03-27 03:51:32 +0900

    Plugin の変更

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -6,10 +6,13 @@ import (
 	"github.com/slack-go/slack"
 )
 
-// Plugin はプラグインが満たすべきインターフェースです。Respond で *slack.RTMEvent を
-// 受け取って，*slack.Client を用いて返事をするなりなんなりします。
+// Plugin はプラグインが満たすべきインターフェースです。
+// Start() で起動処理をします。
+// Respond で *slack.RTMEvent を受け取って，*slack.Client を用いて返事をするなり
+// なんなりします。
 // Stop() で終了処理をします。
 type Plugin interface {
-	Respond(context.Context, *slack.Client, *slack.RTMEvent)
-	Stop()
+	Start() error
+	Respond(context.Context, *slack.Client, *slack.RTMEvent) error
+	Stop() error
 }
