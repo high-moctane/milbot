777740a573831c812f0fa09c60a810a7352863e5
high-moctane <high.moctane@gmail.com>
2020-04-03 02:27:20 +0900

    Makefile.raspi.mk を削除

--- Makefile.raspi.mk
+++
@@ -1,9 +0,0 @@
-.PHONY: install uninstall
-
-install:
-	wget -P /etc/systemd/system/ https://raw.githubusercontent.com/high-moctane/milbot/master/milbot.service
-	systemctl enable milbot.service
-
-uninstall:
-	systemctl disable milbot.service
-	rm -f /etc/systemd/system/milbot.service
